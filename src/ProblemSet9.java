5f5852efadd9cff98c483dafe76d1067ff77c3b2
jackw528 <43789827+jackw528@users.noreply.github.com>
2021-04-12 23:16:44 -0400

    Update ProblemSet9.java

--- src/ProblemSet9.java
+++ src/ProblemSet9.java
@@ -10,7 +10,6 @@ public class ProblemSet9 {
         if(arr == null){
             return null;
         }
-
         int[] evenOddOut = new int[arr.length];
         for (int i = 0;i < arr.length; i++ ){
 
